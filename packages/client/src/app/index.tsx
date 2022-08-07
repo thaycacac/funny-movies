9438b2d21b6dc0cd112085b49d2442af0c5e9e0b
thaycacac <thaycacac@gmail.com>
2022-08-07 22:10:41 +0700

    feat(ui): add mui

---
+++ packages/client/src/app/index.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@mui/material';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Button variant="contained">Hello World</Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
